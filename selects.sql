c04eec03e8ecc06c2dfc6266d11fada9a0fd6bcd
Diones Rossetto <dnrost@gmail.com>
2016-07-17 14:09:44 -0300

    Create selects.sql

---
+++ selects.sql
@@ -0,0 +1,18 @@
+-- tests with any and some
+-- IN- Equal to any member in the list
+-- ANY- Compare value to **each** value returned by the subquery
+-- ALL- Compare value to **EVERY** value returned by the subquery
+
+-- <ANY() - less than maximum
+-- >ANY() - more than minimum
+-- =ANY() - equivalent to IN
+-- >ALL() - more than the maximum
+-- <ALL() - less than the minimum
+
+-- returns all names unless the oldest one 
+select u.name from users_test u
+where u.age < some(select t.age from users_test t)
+
+-- returns all names unless the youngest one 
+select u.name from users_test u
+where u.age > some(select t.age from users_test t)
