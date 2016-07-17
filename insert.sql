221744f5b4af4c816ce3dc3d01085b97fdfbcd0d
Diones Rossetto <dnrost@gmail.com>
2016-07-17 14:05:37 -0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,6 @@
+-- insert some data into table
+insert into users_test (name, age) values('Newton', 10);
+insert into users_test (name, age) values('Keppler', 11);
+insert into users_test (name, age) values('Plato', 12);
+insert into users_test (name, age) values('Galileo', 13);
+insert into users_test (name, age) values('Locke', 14);
