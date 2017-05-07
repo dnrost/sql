a1947cff721c34a0056929fcde81991c80aa0af2
Rossetto <dnrost@gmail.com>
2017-05-07 20:58:56 -0300

    m
    Signed-off-by: Rossetto <dnrost@gmail.com>

--- insert.sql
+++ insert.sql
@@ -4,3 +4,4 @@ insert into users_test (name, age) values('Keppler', 11);
 insert into users_test (name, age) values('Plato', 12);
 insert into users_test (name, age) values('Galileo', 13);
 insert into users_test (name, age) values('Locke', 14);
+insert into users_test (name, age) values('Adam', 15);
