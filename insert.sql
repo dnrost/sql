d08a96f405936abc5f35a9e5d559b96042c1d220
Rossetto <dnrost@gmail.com>
2017-08-20 20:40:00 -0300

    adding

--- insert.sql
+++ insert.sql
@@ -6,3 +6,4 @@ insert into users_test (name, age) values('Galileo', 13);
 insert into users_test (name, age) values('Locke', 14);
 insert into users_test (name, age) values('Adam', 15);
 insert into users_test (name, age) values('Adam36', 15);
+insert into users_test (name, age) values('Adam312222', 17);
